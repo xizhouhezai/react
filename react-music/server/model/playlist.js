1c3d0d6b1fd6de2998995c4a2464fe3a6b206523
yrui <yrui@laixue365.com>
2018-05-16 11:32:14 +0800

    update

--- react-music/server/model/playlist.js
+++ react-music/server/model/playlist.js
@@ -3,6 +3,7 @@ const {
 } = require('../util/api')
 
 module.exports = {
+  // 所有歌单的分类
   async playlist_catlist (ctx, next) {
     const cookie = ctx.cookies.get('Cookie') ? ctx.cookies.get('Cookie') : ''
     const data = {
@@ -15,7 +16,28 @@ module.exports = {
       data,
       cookie
     )
-    ctx.body = result
+    ctx.body = result.body
+    next()
+  },
+  // 歌单分类列表的详情
+  async playlist(ctx, next) {
+    const cookie = ctx.cookies.get('Cookie') ? ctx.cookies.get('Cookie') : ''
+    // order可为 'hot' 可为 'new'
+    const data = {
+      cat: ctx.query.cat || '全部',
+      order: ctx.query.order || 'hot',
+      offset: ctx.query.offset || 0,
+      total: ctx.query.total ? 'true' : 'false',
+      limit: ctx.query.limit || 50
+    }
+    const result = await createWebAPIRequest(
+      'music.163.com',
+      '/weapi/playlist/list',
+      'POST',
+      data,
+      cookie
+    )
+    ctx.body = result.body
     next()
   }
 }
--- react-music/server/router/index.js
+++ react-music/server/router/index.js
@@ -1,9 +1,13 @@
 const Router = require('koa-router')
 
-const playlist = require('../model/playlist')
+const Playlist = require('../model/playlist')
 
 const router = new Router()
 
-router.get('/playlist/catlist', playlist.playlist_catlist)
+// 所有歌单分类列表
+router.get('/playlist/catlist', Playlist.playlist_catlist)
+
+// 歌单分类列表的详情
+router.get('/playlist', Playlist.playlist)
 
 module.exports = router
